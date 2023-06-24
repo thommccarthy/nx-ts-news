f54a1ec0c6b056c2c9274e4cc9f31a246f094740
thommccarthy <mccarttj@gmail.com>
2023-06-24 14:20:47 -0400

    install next-seo and implement default component

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,37 @@
 import Layout from '@/components/layout/Layout';
 import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
+import { DefaultSeo } from 'next-seo';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <>
+      <DefaultSeo
+        title='Thom McCarthy | UX Engineer'
+        description='I build and design responsive, scalable, robust and accessible web interfaces. On occasion I will write about it.'
+        canonical='https://thommccarthy.dev/'
+        openGraph={{
+          url: 'https://thommccarthy.dev/',
+          title: 'Thom McCarthy | UX Engineer',
+          description:
+            'I build and design responsive, scalable, robust and accessible web interfaces. On occasion I will write about it.',
+          images: [
+            {
+              url: 'https://thommccarthy.dev/assets/global/thommccarthy-og-image.jpg',
+              alt: 'Thom McCarthy',
+            },
+          ],
+          site_name: 'Thom McCarthy | UX Engineer',
+        }}
+        twitter={{
+          handle: '@mccarthy_thom',
+          site: '@mccarthy_thom',
+          cardType: 'summary_large_image',
+        }}
+      />
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </>
   );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,6 +6,7 @@ import { InferGetStaticPropsType, NextPage } from 'next';
 import { readPostsInfo } from '../../lib/helper';
 import { PostApiResponse } from '../../utils/types';
 import RainbowDivider from '@/components/global/decorative/RainbowDivider';
+import { NextSeo } from 'next-seo';
 
 export const getStaticProps = async () => {
   const postInfo: PostApiResponse = readPostsInfo();
@@ -26,44 +27,46 @@ type Props = InferGetStaticPropsType<typeof getStaticProps>;
 
 const Home: NextPage<Props> = ({ posts }) => {
   return (
-    <div className={`flex min-h-screen  flex-col p-4 mx-auto`}>
-      <h1 className=' font-black text-center mb-1 md:mb-3 font-fira-code'>
-        <span className='retro'>Thom McCarthy</span>
-      </h1>
-      <h2 className='font-normal text-center mb-8 md:mb-11'>UX Engineer</h2>
-      {/* about */}
-      <div className='border-indigo-800 backgroundGlass border-2 px-3 md:px-9 rounded-lg max-w-4xl mx-auto w-100 flex justify-center items-center py-8  shadow-indigo-500 shadow-lg mb-6 md:mb-8'>
-        <div className='text-center md:text-left'>
-          <p className='text-lg md:text-xl font-normal md:pr-5'>
-            I{`'`}m a <span className='font-black'>UX Engineer</span> based in{' '}
-            <span>West Philadelphia</span>
-            <br />
-            <br />I build and design responsive, scalable, robust and accessible
-            web interfaces. On occasion I will write{' '}
-            <span className='whitespace-nowrap'>about it.</span>
-            <br />
-          </p>
-          <div className='mt-4'>
-            <Link
-              href='/about'
-              className='mt-2 underline text-xl font-bold decoration-sky-300'
-            >
-              About Me &rarr;
-            </Link>
+    <>
+      <div className={`flex min-h-screen  flex-col p-4 mx-auto`}>
+        <h1 className=' font-black text-center mb-1 md:mb-3 font-fira-code'>
+          <span className='retro'>Thom McCarthy</span>
+        </h1>
+        <h2 className='font-normal text-center mb-8 md:mb-11'>UX Engineer</h2>
+        {/* about */}
+        <div className='border-indigo-800 backgroundGlass border-2 px-3 md:px-9 rounded-lg max-w-4xl mx-auto w-100 flex justify-center items-center py-8  shadow-indigo-500 shadow-lg mb-6 md:mb-8'>
+          <div className='text-center md:text-left'>
+            <p className='text-lg md:text-xl font-normal md:pr-5'>
+              I{`'`}m a <span className='font-black'>UX Engineer</span> based in{' '}
+              <span>West Philadelphia</span>
+              <br />
+              <br />I build and design responsive, scalable, robust and
+              accessible web interfaces. On occasion I will write{' '}
+              <span className='whitespace-nowrap'>about it.</span>
+              <br />
+            </p>
+            <div className='mt-4'>
+              <Link
+                href='/about'
+                className='mt-2 underline text-xl font-bold decoration-sky-300'
+              >
+                About Me &rarr;
+              </Link>
+            </div>
           </div>
+          <Image
+            src='/assets/index/Thom_headshot.jpg'
+            alt=''
+            width={200}
+            height={200}
+            className={`rounded-full hidden md:block ${styles.bgGradientImg}`}
+          />
         </div>
-        <Image
-          src='/assets/index/Thom_headshot.jpg'
-          alt=''
-          width={200}
-          height={200}
-          className={`rounded-full hidden md:block ${styles.bgGradientImg}`}
-        />
-      </div>
-      <RainbowDivider />
+        <RainbowDivider />
 
-      <LatestPostsHome posts={posts} />
-    </div>
+        <LatestPostsHome posts={posts} />
+      </div>
+    </>
   );
 };
 
